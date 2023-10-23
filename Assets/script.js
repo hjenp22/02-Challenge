c2fb0adf47f4ae45cc2c042e83b6bd9c95614272
hjenp22 <hjipark22@gmail.com>
2023-10-23 08:35:26 -0700

    added and changed js script

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,3 @@
-//code for homework here
 var generate = document.querySelector("#generate");
 
 function writePassword() {
@@ -8,51 +7,64 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var minLength = parseInt(
-    prompt("Enter the length of the password between 8 and 128 characters"));
-  var maxLength = 128;
+  var minLength = parseInt(prompt("Enter the length of the password between 8 and 128 characters"));
+
+  var isValidLength = !isNaN(minLength) && minLength >= 8 && minLength <= 128;
+
+  if (!isValidLength) {
+    alert("Please enter a valid password length between 8 and 128 characters");
+  }
+
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  var allChars = buildCharacterSet(includeLowercase, includeUppercase, includeNumeric, includeSpecial);
+  var isValidChars = allChars !== "";
+
+  if (!isValidChars) {
+    alert("Please select at least one character type for the password.");
+  }
+
+  if (isValidLength && isValidChars) {
+    return generateRandomPassword(allChars, minLength);
+  }
 }
 
-if (isNaN(minLength) || minLength < 8 || minLength > 128) {
-  alert("Please enter a valid password length between 8 and 128 characters");
-  return; 
-}
+function buildCharacterSet(includeLowercase, includeUppercase, includeNumeric, includeSpecial) {
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*_()+-=[]{}|;:,.<>?";
 
-var includeLowercase = confirm("Include lowercase characters?");
-var includeUppercase = confirm("Include uppercase characters?");
-var includeNumeric = confirm("Include numeric characters?");
-var includeSpecial = confirm("Include special characters?");
+  var allChars = "";
 
-var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericChars = "0123456789";
-var specialChars = "!@#$%^&*_()+-=[]{}|;:,.<>?";
+  if (includeLowercase) {
+    allChars += lowercaseChars;
+  }
+  if (includeUppercase) {
+    allChars += uppercaseChars;
+  }
+  if (includeNumeric) {
+    allChars += numericChars;
+  }
+  if (includeSpecial) {
+    allChars += specialChars;
+  }
 
-var allChars = "";
-var password = "";
-
-if (includeLowercase) {
-  allChars += lowercaseChars;
-}
-if (includeUppercase) {
-  allChars += uppercaseChars;
-}
-if (includeNumeric) {
-  allChars += numericChars;
-}
-if (includeSpecial) {
-  allChars += specialChars;
-}
-
-if (allChars === "") {
-  alert("Please select one character type for password.");
   return allChars;
 }
 
-for (var i = 0; i < passwordLength; i++) {
-  var randomIndex = Math.floor(Math.random() * charset.length);
-  password += charset.charAt(randomIndex);
-}
-  return password;
+function generateRandomPassword(allChars, minLength) {
+  var password = "";
 
-  writePassword();
+  for (var i = 0; i < minLength; i++) {
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+generate.addEventListener("click", writePassword);
